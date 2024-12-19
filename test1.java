50eb515f7c3c25559ded2b434fa2944b86b3719f
makotoPACO <makoto.fong26@ycdsbk12.ca>
2024-12-19 09:15:14 -0500

    Test1.java
    This is a test

---
+++ test1.java
@@ -0,0 +1,52 @@
+import arc.*;
+
+public class test1 {
+    public static void main(String[] args) {
+		Console con = new Console();
+
+
+// github pass:  Gywmeg-namxes-9rithy
+//Users/fongm8851/Documents/GitHub/CPT-Makoto
+
+
+con.println("_____");
+con.println("  |");
+con.println("  O");
+
+
+con.println("_____");
+con.println("  |");
+con.println("  O");
+con.println(" [ ]");
+
+
+
+con.println("_____");
+con.println("  |");
+con.println("  O");
+con.println(" [ ]");
+con.println("   |");
+
+
+con.println("_____");
+con.println("  |");
+con.println("  O");
+con.println(" [ ]");
+con.println(" | |");
+
+
+con.println("_____");
+con.println("  |");
+con.println("  O");
+con.println("([ ]");
+con.println(" | |");
+
+
+con.println("_____");
+con.println("  |");
+con.println("  O");
+con.println("([ ])");
+con.println(" | |");
+
+		}
+}
